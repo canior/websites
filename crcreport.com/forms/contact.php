3a700859b066466fe286af1085e301dba072eb18
wade <wade00907@gmail.com>
2023-11-12 01:46:10 -0500

    update contact

--- crcreport.com/forms/contact.php
+++ crcreport.com/forms/contact.php
@@ -1,19 +1,13 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
 $logEntry = [
     'name' => $_POST['name'],
     'email' => $_POST['email'],
     'subject' => $_POST['subject'],
+    'message' => $_POST['message'],
 ];
 $basePah = '/var/www/html/websites/files/crcreport.com/contact/' . time();
-mkdir($basePah, 0777,true);
+mkdir($basePah, 0777, true);
 $logFile = $basePah . '/contact.log'; // Specify the log file path
-
 file_put_contents($logFile, json_encode($logEntry), FILE_APPEND | LOCK_EX);
+echo "OK";
 ?>
